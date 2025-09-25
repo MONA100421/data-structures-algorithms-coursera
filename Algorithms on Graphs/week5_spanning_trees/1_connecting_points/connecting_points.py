a6a647d59dbe25cd02144bb22d06b504576e054b
MONA100421 <wengchen@usc.edu>
2025-09-24 19:00:26 -0700

    update

--- Algorithms on Graphs/week5_spanning_trees/1_connecting_points/connecting_points.py
+++ Algorithms on Graphs/week5_spanning_trees/1_connecting_points/connecting_points.py
@@ -1,16 +1,35 @@
-#Uses python3
+# Uses python3
 import sys
 import math
 
 def minimum_distance(x, y):
-    result = 0.
-    #write your code here
+    n = len(x)
+    used = [False] * n
+    key = [float('inf')] * n
+    key[0] = 0.0
+    result = 0.0
+
+    for _ in range(n):
+        u = -1
+        u_key = float('inf')
+        for v in range(n):
+            if not used[v] and key[v] < u_key:
+                u, u_key = v, key[v]
+
+        used[u] = True
+        result += u_key
+
+        for v in range(n):
+            if not used[v]:
+                d = math.hypot(x[u] - x[v], y[u] - y[v])
+                if d < key[v]:
+                    key[v] = d
+
     return result
 
 
 if __name__ == '__main__':
-    input = sys.stdin.read()
-    data = list(map(int, input.split()))
+    data = list(map(int, sys.stdin.read().split()))
     n = data[0]
     x = data[1::2]
     y = data[2::2]
--- Algorithms on Graphs/week5_spanning_trees/2_clustering/clustering.py
+++ Algorithms on Graphs/week5_spanning_trees/2_clustering/clustering.py
@@ -1,15 +1,53 @@
-#Uses python3
+# Uses python3
 import sys
 import math
 
+class DSU:
+    def __init__(self, n):
+        self.parent = list(range(n))
+        self.rank = [0] * n
+
+    def find(self, a):
+        if self.parent[a] != a:
+            self.parent[a] = self.find(self.parent[a])
+        return self.parent[a]
+
+    def union(self, a, b):
+        a, b = self.find(a), self.find(b)
+        if a == b:
+            return False
+        if self.rank[a] < self.rank[b]:
+            a, b = b, a
+        self.parent[b] = a
+        if self.rank[a] == self.rank[b]:
+            self.rank[a] += 1
+        return True
+
+
 def clustering(x, y, k):
-    #write your code here
-    return -1.
+    n = len(x)
+    edges = []
+    for i in range(n):
+        for j in range(i + 1, n):
+            w = math.hypot(x[i] - x[j], y[i] - y[j])
+            edges.append((w, i, j))
+    edges.sort(key=lambda e: e[0])
+
+    dsu = DSU(n)
+    clusters = n
+
+    for w, u, v in edges:
+        if dsu.find(u) != dsu.find(v):
+            if clusters == k:
+                return w
+            dsu.union(u, v)
+            clusters -= 1
+
+    return 0.0
 
 
 if __name__ == '__main__':
-    input = sys.stdin.read()
-    data = list(map(int, input.split()))
+    data = list(map(int, sys.stdin.read().split()))
     n = data[0]
     data = data[1:]
     x = data[0:2 * n:2]
